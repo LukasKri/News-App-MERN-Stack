6b209df69a8745b3a4b8f230862c7d771df81c50
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-07-12 23:22:53 +0300

    Article URLs goes to the server, now need to implement MongoDB.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-var searchArray = [];
+const searchValuesArray = [];
+
+const clickedArticlesArray = [];
 
 app.use(express.json());
 
@@ -12,13 +14,22 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.post("/search", (req, res) => {
+app.post("/searchValue", (req, res) => {
     const searchValue = req.body;
 
-    console.log("I got a request!");
+    console.log("I got a searchValue request!");
     console.log(searchValue);
-    searchArray.push(searchValue);
-    console.log(searchArray);
+    searchValuesArray.push(searchValue);
+    console.log(searchValuesArray);
+});
+
+app.post("/clickedArticle", (req, res) => {
+    const clickedArticle = req.body;
+
+    console.log("I got a clickedArticle request!");
+    console.log(clickedArticle);
+    clickedArticlesArray.push(clickedArticle);
+    console.log(clickedArticlesArray);
 });
 
 app.listen(PORT, () => {
