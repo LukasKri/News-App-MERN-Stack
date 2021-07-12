4b7908470a953c205703806279b7788bd4b398ad
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-07-12 22:15:33 +0300

    Search value goes  to the server, now need to implement clicked article URL sending.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+var searchArray = [];
+
+app.use(express.json());
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
+app.post("/search", (req, res) => {
+    const searchValue = req.body;
+
+    console.log("I got a request!");
+    console.log(searchValue);
+    searchArray.push(searchValue);
+    console.log(searchArray);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
